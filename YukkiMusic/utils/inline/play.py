fdf3b50d0da81157b8b095a7ced5784097564e69
ʙᴧɢᴧsҡᴧʀᴧ-ᴇx <93908715+ybgskr12@users.noreply.github.com>
2022-10-24 20:13:06 +0700

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -17,7 +17,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 text=_["PL_B_3"],  switch_inline_query_current_chat=""
             ),
             InlineKeyboardButton(
-                text=_["S_B_9"], url=f"https://t.me/sinikackjoin",
+                text=_["S_B_9"], url=f"https://t.me/ybgskr_ex",
             ),
         ],
         [
@@ -69,10 +69,10 @@ def stream_markup(_, videoid):
         ],
         [
             InlineKeyboardButton(
-                text=_["S_B_9"], url=f"https://t.me/sinikackjoin",
+                text=_["S_B_9"], url=f"https://t.me/ybgskr_ex",
             ),
             InlineKeyboardButton(
-                text=_["S_B_9"], url=f"https://t.me/jasmerArea",
+                text=_["S_B_9"], url=f"https://t.me/loveisfuckedup",
             ),
         ],
         [
